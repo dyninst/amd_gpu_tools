e92b7fd5bace393d7194e78a3dc51952536e1298
Ronak Chauhan <ronak@cs.wisc.edu>
2026-05-01 14:45:03 -0500

    Update extract-fatbin to take output argument

--- extract-fatbin.cpp
+++ extract-fatbin.cpp
@@ -4,6 +4,9 @@
 
 #include "elfio/elfio.hpp"
 
+// usage:
+// extract-fatbin <exec> <output-fatbin>
+
 static ELFIO::section *getSection(const std::string &sectionName, const ELFIO::elfio &file) {
   for (const auto &section: file.sections)  {
     if (section->get_name() == sectionName) {
@@ -18,25 +21,27 @@ static ELFIO::section *getFatbinSection(const ELFIO::elfio &file) {
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <executable file with fatbin>" << std::endl;
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <executable file with fatbin> <output fatbin name>" << std::endl;
     return 1;
   }
 
+  std::string execFilePath(argv[1]);
+  std::string outputFatbinPath(argv[2]);
   ELFIO::elfio execFile;
-  if (!execFile.load(argv[1])) {
-    std::cerr << "can't find or process ELF file " << argv[1] << '\n';
+  if (!execFile.load(execFilePath)) {
+    std::cerr << "can't find or process ELF file " << execFilePath << '\n';
     exit(1);
   }
 
   ELFIO::section *fatbinSection = getFatbinSection(execFile);
   if (!fatbinSection) {
-    std::cerr << ".hip_fatbin section not found in " << argv[1] << "\n";
+    std::cerr << ".hip_fatbin section not found in " << execFilePath << "\n";
     exit(1);
   }
 
   // Write fatbin to a separate file
-  std::ofstream fatbinFile(std::string(argv[1]) + ".fatbin", std::ios::out | std::ios::binary);
+  std::ofstream fatbinFile(outputFatbinPath, std::ios::out | std::ios::binary);
 
   fatbinFile.write(fatbinSection->get_data(), fatbinSection->get_size());
 
